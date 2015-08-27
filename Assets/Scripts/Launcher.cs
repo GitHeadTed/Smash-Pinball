2e82c8d2fb6d205807e285c0eb344ead12b4810e
Theodore Park <theohpark@gmail.com>
2015-08-26 23:18:33 -0700

    Added launcher and gate

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour {
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionStay (Collision col){
+		print ("collided with object");
+		if (Input.anyKeyDown) {
+			col.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,force));
+		}
+	}
+}
