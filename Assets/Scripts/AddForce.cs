4f4bce08a7325aa0abe9881b35083e9388d4e152
Theodore Park <theohpark@gmail.com>
2015-08-26 22:33:35 -0700

    First commit

---
+++ Assets/Scripts/AddForce.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForce : MonoBehaviour {
+	public float force = 1000;
+
+	private Rigidbody body;
+
+	void Awake () {
+		body = GetComponent<Rigidbody> ();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			body.AddForce(new Vector3(0,0,force));
+		}
+	}
+}
